275e500701b2edf8ded6a3cf7facb4defbad7670
mukil013 <mukilmukil1947@gmail.com>
2023-07-18 10:43:09 +0530

    comments

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
+//andha html file dummy number irunthuchula adha inga call pandrom
 let a = document.querySelector('#displayHome');
 let b = document.querySelector('#displayAway');
+
+//aprm goal nu oru variable create panni initialize pandrom
 let goal = 0;
 
 
@@ -7,7 +10,13 @@ let goal = 0;
 function AddGoadHome(){
     goal += 1;
     a.textContent = goal;
+    /*inga yepdi na namma andha dummy text ah a nu define pannirukoma
+    appo andha mathanum na indha textContent use pannanum namma goal ah + 1 pandrom
+    ah starting goal 0 la irunthu apdiye 1 increment aagum */
     console.log("Home Scored");
+    /*idhu summa work aagutha illiya nu paaka browser la right click panna inspect nu
+    oru option varum adhu open aana apro console oru tab varum adhula goal button press 
+    panna indha message varum avalotha*/
 }
 
 function AddGoadAway(){
@@ -17,7 +26,10 @@ function AddGoadAway(){
 }
 
 function reset(){
+    /*ipo namma goal variable ku again 0 kudukurom reset panna idhu click aana 
+    goal oda value 0 ku poirum*/
     goal = 0;
+    /*inga same mela panna maari tha*/
     a.textContent = goal;
     b.textContent = goal;    
 }
